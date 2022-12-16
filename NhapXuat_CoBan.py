0e06910a572ad323eb5d545db7c5bdf9afa70df0
GiaBao0510 <pgiabao2002@gmail.com>
2022-12-16 15:45:36 +0700

    Gioi thieu cau lenh print & input

---
+++ NhapXuat_CoBan.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 15:21:56 2022
+
+@author: Gia Bao
+"""
+#Nhap
+print("\tNhap thong tin sinh vien va ban:")
+hten = input("Enter your full name: ")
+tuoi = input("Enter your age: ")
+mssv = input("Enter your student number: ")
+sex = input("Enter your gender: ")
+#IN
+print("--------------------Your information---------------------")
+print("Name:"+hten)
+print("Age:"+tuoi)
+print("Student number:"+mssv)
+print("Sex:"+sex)
